3d196a1efd23a29d982479b85fd52378f519ef52
Kavish Rizvi <rizvikavish205.com>
2021-09-30 11:28:34 +0530

    first commit

---
+++ src/main/java/com/hysterixexample/microservicewithhysterix/MicroserviceWithHysterixApplication.java
@@ -0,0 +1,13 @@
+package com.hysterixexample.microservicewithhysterix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceWithHysterixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceWithHysterixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hysterixexample/microservicewithhysterix/MicroserviceWithHysterixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hysterixexample.microservicewithhysterix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceWithHysterixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
